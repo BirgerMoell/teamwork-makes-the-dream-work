de326011b3d0963af3df0729510dc0bcac1796c2
Birger Moëll <birger.moell@gmail.com>
2020-03-05 23:37:58 +0100

    Updates

---
+++ precache-manifest.982c420a9eb519f2f9bfe6ae6d73bda0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22e0dc0d1a9f5c7b141b64d2bd7f189b",
+    "url": "/teamwork-makes-the-dream-work/index.html"
+  },
+  {
+    "revision": "a34e92ac499359f24f86",
+    "url": "/teamwork-makes-the-dream-work/static/css/main.4a5affc1.chunk.css"
+  },
+  {
+    "revision": "01e44a665180968d5728",
+    "url": "/teamwork-makes-the-dream-work/static/js/2.fd342c69.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/teamwork-makes-the-dream-work/static/js/2.fd342c69.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a34e92ac499359f24f86",
+    "url": "/teamwork-makes-the-dream-work/static/js/main.46149303.chunk.js"
+  },
+  {
+    "revision": "7552405536d7b235f928",
+    "url": "/teamwork-makes-the-dream-work/static/js/runtime-main.966a85eb.js"
+  },
+  {
+    "revision": "f6ce7f195762c5c79b4a65891dc93840",
+    "url": "/teamwork-makes-the-dream-work/static/media/teamwork.f6ce7f19.jpeg"
+  }
+]);
